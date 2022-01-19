580c3ec44caa8f9ed30c67a9157e3b2e148e8c4e
M Hafiz B <hafiz.bukhari@hotmail.com>
2022-01-19 21:29:54 +0700

    makeLoginpage

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {BaseUrl} from './Utils/BaseApi'
+import {NativeBaseProvider}from 'native-base'
 import React, {useEffect} from 'react'
-import jwt_decode from 'jwt-decode'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './Page/Login'
 
 export default function App() {
   
-  useEffect(()=>{
-    // BaseUrl.get("/showData").then(e=>console.log(e.data))
-
-    BaseUrl.post("/login",{
-      email:'hafiz@gmail.com',
-      password:'123'
-    }).then(e=>console.log(jwt_decode(e.data.token)))
-    
-  },[])
-
   
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer >
+        <Stack.Navigator screenOptions ={{headerShown:false}}>
+          <Stack.Screen name="login" component={Login}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+    
   );
 }
 
