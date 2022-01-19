b54e7ede4aab56a9f4dfb6f8379d568c1d96a14e
M Hafiz B <hafiz.bukhari@hotmail.com>
2022-01-19 21:45:57 +0700

    login fix

--- Page/Login.js
+++ Page/Login.js
@@ -1,19 +1,27 @@
-import React, {useEffect} from 'react'
-import {} from 'react-native'
+import React, {useEffect, useState } from 'react'
+import {AsyncStorage} from 'react-native'
 import {Center, Input, Stack, Button} from 'native-base'
 import {BaseUrl} from '../Utils/BaseApi'
 import jwt_decode from 'jwt-decode'
 
 export default function Login() { 
+    const [data, setdata] = useState({
+        email:"",
+        password:""
+    })
 
     useEffect(()=>{
         // BaseUrl.get("/showData").then(e=>console.log(e.data))
-        BaseUrl.post("/login",{
-          email:'hafiz@gmail.com',
-          password:'123'
-        }).then(e=>console.log(jwt_decode(e.data.token)))
+        // BaseUrl.post("/login",{
+        //   email:'hafiz@gmail.com',
+        //   password:'123'
+        // }).then(e=>console.log(jwt_decode(e.data.token)))
         
       },[])
+
+    function HandleSubmit(){
+         BaseUrl.post("/login", data).then(e=>console.log(jwt_decode(e.data.token)))
+    }
     return(
         <>
             <Center flex={1} backgroundColor="white">
@@ -28,9 +36,14 @@ export default function Login() {
                         }}
                         space = {4}
                     >
-                    <Input fontSize={20} variant="underlined" placeholder="Email" />
-                    <Input fontSize={20} variant="underlined" placeholder="Password" />
-                    <Button size="lg" >  Submit </Button>
+                    <Input fontSize={20} variant="underlined" placeholder="Email" 
+                        onChangeText={e=>setdata({...data,email:e})}
+                    />
+                    <Input fontSize={20} variant="underlined" placeholder="Password" 
+                        onChangeText={e=>setdata({...data,password:e})}
+                        secureTextEntry={true}
+                    />
+                    <Button size="lg" onPress={HandleSubmit} >  Submit </Button>
 
                     </Stack>
                 </Center>
