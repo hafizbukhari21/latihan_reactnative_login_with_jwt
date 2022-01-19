e1ea3f120651e97745c1c359fd4ef5451d7c8353
M Hafiz B <hafiz.bukhari@hotmail.com>
2022-01-19 20:43:00 +0700

    install native base

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ export default function App() {
     }).then(e=>console.log(jwt_decode(e.data.token)))
     
   },[])
+
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
